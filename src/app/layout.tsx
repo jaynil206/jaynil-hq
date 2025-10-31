3fed284bec2a3a74512a034acdda26bebbbbe7be
Jaynil Patel <jaynil206@outlook.com>
2025-10-31 23:21:28 +0000

    fix: change image and wording

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,14 +10,14 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "BREAKING: Man Runs Far. Needs Donations To Justify It.",
-  description: "next year i (jaynil) am running the marathon to raise money for lupus uk in memory of hiral patel (my mum). please click to donate, find out more, and keep up to date with how it goes.",
+  title: "BREAKING: Local Man Starts Running. Needs Donations To Justify It.",
+  description: "next year, i (Jaynil) am running the marathon to raise money for Lupus UK in memory of Hiral Patel (my mum). please click to donate, find out more, and keep up to date with how it goes.",
   openGraph: {
-    title: "BREAKING: Man Runs Far. Needs Donations To Justify It.",
-    description: "next year i (jaynil) am running the marathon to raise money for lupus uk in memory of hiral patel (my mum). please click to donate, find out more, and keep up to date with how it goes.",
+    title: "BREAKING: Local Man Starts Running. Needs Donations To Justify It.",
+    description: "next year, i (Jaynil) am running the marathon to raise money for Lupus UK in memory of Hiral Patel (my mum). please click to donate, find out more, and keep up to date with how it goes.",
     images: [
       {
-        url: "https://www.jaynil.xyz/images/meme-min.png",
+        url: "https://www.jaynil.xyz/images/meme-min-2.png",
         width: 1200,
         height: 630,
       },
