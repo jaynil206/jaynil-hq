cddd86b384bd77889aa0402229d4c26f61c71b13
Jaynil Patel <jaynil206@outlook.com>
2025-10-31 18:01:46 +0000

    fix: opengraph image

--- src/app/jaynil/page.tsx
+++ src/app/jaynil/page.tsx
@@ -1,4 +1,5 @@
 import SplitPaneLayout from '../components/SplitPaneLayout';
+import Image from 'next/image';
 
 export default function JaynilPage() {
   return (
@@ -6,6 +7,7 @@ export default function JaynilPage() {
       <div className="text-left">
         <h1 className="text-base font-medium mb-4">About Jaynil</h1>
         <p className="text-base">Learn more about Jaynil&apos;s journey and motivation for running the London Marathon.</p>
+        <Image src="/images/meme.png" alt="Jaynil" width={1200} height={630}/>
       </div>
     </SplitPaneLayout>
   );
