73ba6aada42183805f1288a5ea89bd849c0589a1
Jaynil Patel <jaynil206@outlook.com>
2025-10-31 18:12:22 +0000

    fix: brought image to under 600kb

--- src/app/jaynil/page.tsx
+++ src/app/jaynil/page.tsx
@@ -1,5 +1,4 @@
 import SplitPaneLayout from '../components/SplitPaneLayout';
-import Image from 'next/image';
 
 export default function JaynilPage() {
   return (
@@ -7,7 +6,6 @@ export default function JaynilPage() {
       <div className="text-left">
         <h1 className="text-base font-medium mb-4">About Jaynil</h1>
         <p className="text-base">Learn more about Jaynil&apos;s journey and motivation for running the London Marathon.</p>
-        <Image src="/images/meme.png" alt="Jaynil" width={1200} height={630}/>
       </div>
     </SplitPaneLayout>
   );
